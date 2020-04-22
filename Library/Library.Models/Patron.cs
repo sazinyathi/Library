862d0211e6f2bc7a0d55625a7e97806694ce2f10
Saz <sazi.nyathi@lexisnexis.co.za>
2020-04-22 23:39:48 +0200

    DEV: Initial Create Library Application

---
+++ Library/Library.Models/Patron.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Library.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string  Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime  DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+
+
+    }
+}
---
+++ Library/LibraryData/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryData
+{
+    public class LibraryContext:DbContext
+    {
+        public LibraryContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Patron> Patrons { get; set; }
+    }
+}
